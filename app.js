a5a910397ca6e8ddad431f3e5f61b50c5da299e8
test <test@localhost.localdomain>
2018-04-12 15:13:03 +0900

    profile

--- app.js
+++ app.js
@@ -27,17 +27,6 @@ app.use( methodOverride(function(req, res){
 //routing
 
 app.get('/', post.index);
-app.get('/company', post.company);
-app.get('/workFlow', post.workFlow);
-app.get('/contact', post.contact);
-app.get('/recruit', post.recruit);
-app.get('/bbs', post.bbs);
-app.get('/posts/:id([0-9]+)', post.show);
-app.get('/posts/new', post.new);
-app.post('/posts/create', post.create);
-app.get('/posts/:id/edit', post.edit);
-app.put('/posts/:id', post.update);
-app.delete('/posts/:id', post.destroy);
 
 app.listen(3000);
 console.log("server starting..."+__dirname );
--- routes/post.js
+++ routes/post.js
@@ -1,52 +1,5 @@
 
-var fs = require('fs');
-var json = fs.readFileSync("article.json", "utf-8");
-var obj = JSON.parse(json);
-var items = obj.article;
-
 exports.index = function(req, res) {
-    res.render('posts/index');
+    res.render('index');
 };
-exports.show = function(req, res) {
-    res.render('posts/show', {post: items[req.params.id]});
-};
-exports.new = function(req, res) {
-    res.render('posts/new');
-};
-exports.edit = function(req, res) {
-    res.render('posts/edit', {post: items[req.params.id], id: req.params.id});
-};
-exports.update = function(req, res) {
-    items[req.body.id] = {
-        title: req.body.title,
-        body: req.body.body
-    };
-    res.redirect('/');
-};
-exports.destroy = function(req, res) {
-    items.splice(req.body.id, 1);
-    res.redirect('/');
-};
-exports.create = function(req, res) {
-    var post = {
-        title: req.body.title,
-        body: req.body.body
-    };
-    items.push(post);
-    res.redirect('/');
-};
-exports.company = function(req, res) {
-    res.render('company');
-};
-exports.workFlow = function(req, res) {
-    res.render('workFlow');
-};
-exports.contact = function(req, res) {
-    res.render('contact');
-};
-exports.recruit = function(req, res) {
-    res.render('recruit');
-};
-exports.bbs = function(req, res) {
-    res.render('bbs', {posts: items});
-};
+
