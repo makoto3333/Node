b76b3d3936d96c62c37442b6a931ca4c97f44db6
test <test@localhost.localdomain>
2018-04-13 11:15:23 +0900

    kansei

--- public/javascripts/app/index.js
+++ public/javascripts/app/index.js
@@ -1,10 +1,9 @@
- $(function(){
-
-$('#portfolio').click(function(){
-	$('#product-portfolio').toggle();
-
-});
-
+ $(function () {
+   $('#portfolio').click(function () {
+     $('#product-portfolio').toggle();
+   });
  });
-
-
+ $('head').append('<style>body{display:none;}');
+ $(window).on("load", function () {
+   $('body').delay(600).fadeIn("slow");
+ });
